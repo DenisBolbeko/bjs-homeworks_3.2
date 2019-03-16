a8fc25176ed77f954f7abad878fc88565e1041db
DenisBolbeko <47342011+DenisBolbeko@users.noreply.github.com>
2019-03-16 19:08:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
-
 // Создание функции для вывода сообщения будильника
-function setDailyRhythm (wakeUpTime, bedTime) {
-  if (wakeUpTime == "7:30") {
-    alert('Доброе утро!');
-  } 
-  if (bedTime == "23:30") {
-    alert('Пора спать!');
-  }
-  
+function setDailyRhythm(wakeUpTime, bedTime) {
+
+    const goodMorning = () => alert('Доброе утро, Вася!');
+    const goodNight = () => alert('Спокойной ночи, Вася!');
+
+    const checkTimewakeUp = setAlarm(wakeUpTime, goodMorning);
+    const checkTimetoSleep = setAlarm(bedTime, goodNight);
+
+    setInterval(checkTimewakeUp, 10000);
+    setInterval(checkTimetoSleep, 10000);
 }
 
-// Создание функции для определения текущего времени
-function checkTime()
-{
-const timeNow = new Date();
-const hours = timeNow.getHours();
-const minutes = timeNow.getMinutes();
-const result = hours + ':' + minutes;
-return result;
+// Создание функции которая возвращает в качестве результата другую функцию, в которую передаётся системное время
+
+function setAlarm(time, callback) {
+    return function () {
+        const timeNow = new Date();
+        const hours = timeNow.getHours().toString();
+        const minutes = timeNow.getMinutes().toString();
+        let resultTime = "";
+//  Добавление нуля к минутам если минут в текущем времени меньше 10
+      if (minutes.length < 2) {
+           resultTime = hours + ":" + 0 + minutes;
+        }  else if (minutes.length >= 2) {
+           resultTime = hours + ":" + minutes;
+        } 
+//       Проверка на совпадение времени
+        if (resultTime === time) {
+            callback();
+        }
+    }
 }
+        
 
-
-// Для автоматического вызова функции каждые 10 сек
-setInterval("setDailyRhythm(checkTime(), checkTime())", 10000)
+setDailyRhythm('7:30','19:07');
